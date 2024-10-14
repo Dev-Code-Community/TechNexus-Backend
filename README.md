dd99301272104e80c86ab23014f0b6701012e9b0
kaustubhdeokar <kaustubhd9@gmail.com>
2024-10-15 00:17:11 +0530

    backend setup node js, routes established, postgres setup, db schema done, db backend api sync pending

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# TechNexus-Backend
+details:
+
+database used: postgres (docker)
+
+- postgres docker image pull:
+    docker pull postgres
+
+- postgres docker container run:
+    sudo docker run --name postgres-nexus -e POSTGRES_USER=postgres -e \ POSTGRES_PASSWORD=nexus -e POSTGRES_DB=nexus_db -p 5432:5432 -d postgres
+
+
+    container name: postgres-nexus
+    database: nexus_db
+    postgres user: postgres
+    postgres password: nexus    
+
+- manipulate database nexus_db:
+    - docker exec -it postgres-container bash
+    - su - postgres
+    - psql -d nexus_db
+    
+
+For each event : 
+  - CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    type VARCHAR(255) DEFAULT 'offline'
+    description TEXT,
+    image VARCHAR(255),
+    link VARCHAR(255)    
+    ); 
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'nexus',
+    host: 'localhost',
+    database: 'nexus_db',
+    password: 'nexus',
+    port: 5432,
+});
+
+// Use CORS to allow requests from the React app
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello from the Node.js backend!');
+});
+
+app.get('/events/all', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM events');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/events/online', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM events');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/events/offline', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM events where eve');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.post('/events', async (req, res) => {
+    try {
+        const result = await pool.query('INSERT INTO events (name, date) VALUES ($1, $2) RETURNING *', ['New event', new Date()]);
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
